a1303f69814785dc5d862cb293ba7eee511bb0cb
ChengIC <39056068+ChengIC@users.noreply.github.com>
2023-06-06 22:49:32 +0100

    multi-threading
    add the speed of chat app

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify, Response
 from utils.graph_qurey import QueryGraph
 import time
 import json
+from threading import Thread
 
 ### Load environment variables
 from dotenv import load_dotenv
@@ -22,8 +23,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 @app.route('/get', methods=['POST'])
-def get_bot_response(): 
+def get_bot_response():
     question = request.form.get('msg')
 
     QG = QueryGraph(neo4j_url, neo4j_user, neo4j_password, 
@@ -31,47 +33,30 @@ def get_bot_response():
     
     response = {}
 
-    try:
-        # Exp 2: Generic Cypher
-        response["Generic_Cypher"] = QG.generic_cypther(question)
-        print ("........Finished Generic Cypher........")
-    except Exception as e:
-        response["Generic_Cypher"] = str(e)
-        print(f"An error occurred in Generic Cypher: {str(e)}")
+    def run_exp(exp_name, func, *args, **kwargs):
+        try:
+            response[exp_name] = func(*args, **kwargs)
+            print (f"........Finished {exp_name}........")
+        except Exception as e:
+            response[exp_name] = str(e)
+            print(f"An error occurred in {exp_name}: {str(e)}")
 
-    try:
-        # Exp 3: Graph Cypher QA
-        response["Graph_Cypher_QA"] = QG.graph_cypher_qa(question)
-        print ("........Finished Graph_Cypher_QA........")
-    except Exception as e:
-        response["Graph_Cypher_QA"] = str(e)
-        print(f"An error occurred in Graph Cypher QA: {str(e)}")
+    experiments = [
+        ("Generic_Cypher", QG.generic_cypther, [question]),
+        ("Graph_Cypher_QA", QG.graph_cypher_qa, [question]),
+        ("Graph_Cypher_QA_with_Graph_in_Pinecone", QG.graph_cypher_qa_pinecone, [question, "context", "graph_02"]),
+        ("Graph_Cypher_QA_with_PDF_in_Pinecone", QG.graph_cypher_qa_pinecone, [question, "text", "unilever"]),
+        ("Graph_Cypher_QA_with_Knowledge_Triples", QG.graph_cypher_qa_knowledge_triples, [question]),
+    ]
 
-    try:
-        # Exp 4: Graph Cypher QA with Sources (Pinecone)
-        response["Graph_Cypher_QA_with_Graph_in_Pinecone"] = QG.graph_cypher_qa_pinecone(question, text_key="context", my_namespace="graph_02")
-        print ("........Finished Graph_Cypher_QA_with_Graph_in_Pinecone........")
-    except Exception as e:
-        response["Graph_Cypher_QA_with_Graph_in_Pinecone"] = str(e)
-        print(f"An error occurred in Graph Cypher QA with Graph in Pinecone: {str(e)}")
+    threads = [Thread(target=run_exp, args=exp) for exp in experiments]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
 
-    try:
-        response["Graph_Cypher_QA_with_PDF_in_Pinecone"] = QG.graph_cypher_qa_pinecone(question, text_key="text", my_namespace="unilever")
-        print ("........Finished Graph_Cypher_QA_with_PDF_in_Pinecone........")
-    except Exception as e:
-        response["Graph_Cypher_QA_with_PDF_in_Pinecone"] = str(e)
-        print(f"An error occurred in Graph Cypher QA with PDF in Pinecone: {str(e)}")
+    return jsonify(response)
 
-    try:
-        # Exp 5: Graph Cypher QA with Triples
-        response["Graph_Cypher_QA_with_Knowledge_Triples"] = QG.graph_cypher_qa_knowledge_triples(question)
-        print ("........Finished Graph_Cypher_QA_with_Knowledge_Triples........")
-    except Exception as e:
-        response["Graph_Cypher_QA_with_Knowledge_Triples"] = str(e)
-        print(f"An error occurred in Graph Cypher QA with Knowledge Triples: {str(e)}")
-
-
-    return jsonify(response) 
 
 
 
