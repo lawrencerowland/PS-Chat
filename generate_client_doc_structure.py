864db35263644dc1829de2edb7876305a1fd2879
Daniel Sullivan <daniels@projectingsuccess.co.uk>
2023-09-08 11:29:00 +0100

    refactor generate_client_doc_structure.py to use imports

--- generate_client_doc_structure.py
+++ generate_client_doc_structure.py
@@ -2,36 +2,7 @@ import os
 import glob
 import json
 
-def get_files(directory_path):
-	files = {}
-	for item in os.listdir(directory_path):
-		if not item.startswith('.'):
-			item_path = os.path.join(directory_path, item)
-			if os.path.isdir(item_path):
-				files[item] = get_files(item_path) or {}
-			else:
-				files[item] = None
-	return files
-
-def get_namespaces(directory):
-	namespaces = []
-	for root, dirs, files in os.walk(directory):
-		for dir in dirs:
-			namespaces.append(dir)
-	return namespaces
-
-def get_questions(directory):
-	Example_Questions = {}
-	# Glob pattern to find 'question.txt' files in all subdirectories
-	for filename in glob.glob(directory + '/**/questions.txt', recursive=True):
-		with open(filename, 'r') as f:
-			# Get the subfolder name
-			subfolder_name = os.path.dirname(filename).split('/')[-1]
-			# Read the file content and store it as a list of lines
-			content = f.read().splitlines()
-			dict_key = f"Example Questions of {subfolder_name}"
-			Example_Questions[dict_key] = content
-	return Example_Questions
+from main import get_files, get_namespaces, get_questions
 
 if __name__ == "__main__":
 	doc_dir = './docs'
@@ -40,7 +11,7 @@ if __name__ == "__main__":
 	questions = get_questions(doc_dir)
 	docs_dict = {'files': files, 'namespaces': namespaces, 'questions': questions}
 	filepath = "./data/client_doc_structure.json"
-	
+
 	os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
 	f = open(filepath, "w")
