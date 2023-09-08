90786baaf30c33a35de1ce95c279ead6170eded1
Daniel Sullivan <daniels@projectingsuccess.co.uk>
2023-09-08 11:31:01 +0100

    refactor to use pdf_doc_dir env variable

--- generate_client_doc_structure.py
+++ generate_client_doc_structure.py
@@ -4,11 +4,12 @@ import json
 
 from main import get_files, get_namespaces, get_questions
 
+pdf_doc_dir = os.getenv('PDF_DOC_DIR')
+
 if __name__ == "__main__":
-	doc_dir = './docs'
-	files = get_files(doc_dir)
-	namespaces = get_namespaces(doc_dir)
-	questions = get_questions(doc_dir)
+	files = get_files(pdf_doc_dir)
+	namespaces = get_namespaces(pdf_doc_dir)
+	questions = get_questions(pdf_doc_dir)
 	docs_dict = {'files': files, 'namespaces': namespaces, 'questions': questions}
 	filepath = "./data/client_doc_structure.json"
 
