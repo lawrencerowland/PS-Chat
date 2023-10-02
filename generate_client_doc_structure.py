9da6c7af43df234b476f63e175ef13e559f00bd0
Daniel Sullivan <daniels@projectingsuccess.co.uk>
2023-10-02 10:29:26 +0100

    move get_xyz functions to file

--- generate_client_doc_structure.py
+++ generate_client_doc_structure.py
@@ -2,9 +2,38 @@ import os
 import glob
 import json
 
-from main import get_files, get_namespaces, get_questions
-
 pdf_doc_dir = os.getenv('PDF_DOC_DIR')
+
+def get_files(directory_path):
+	files = {}
+	for item in os.listdir(directory_path):
+		if not item.startswith('.'):
+			item_path = os.path.join(directory_path, item)
+			if os.path.isdir(item_path):
+				files[item] = get_files(item_path) or {}
+			else:
+				files[item] = None
+	return files
+
+def get_namespaces(directory):
+	namespaces = []
+	for root, dirs, files in os.walk(directory):
+		for dir in dirs:
+			namespaces.append(dir)
+	return namespaces
+
+def get_questions(directory):
+	Example_Questions = {}
+	# Glob pattern to find 'question.txt' files in all subdirectories
+	for filename in glob.glob(directory + '/**/questions.txt', recursive=True):
+		with open(filename, 'r') as f:
+			# Get the subfolder name
+			subfolder_name = os.path.dirname(filename).split('/')[-1]
+			# Read the file content and store it as a list of lines
+			content = f.read().splitlines()
+			dict_key = f"Example Questions of {subfolder_name}"
+			Example_Questions[dict_key] = content
+	return Example_Questions
 
 if __name__ == "__main__":
 	files = get_files(pdf_doc_dir)
